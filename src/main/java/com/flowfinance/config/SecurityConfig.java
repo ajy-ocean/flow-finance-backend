e9a647a366134248369e8b2519768f1290dc2f0a
Ajay Rawat <93385633+ajy-ocean@users.noreply.github.com>
2025-12-16 13:15:07 +0530

    added more config

--- src/main/java/com/flowfinance/config/SecurityConfig.java
+++ src/main/java/com/flowfinance/config/SecurityConfig.java
@@ -11,8 +11,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder; 
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -35,11 +33,6 @@ public class SecurityConfig {
     }
     
     private static final String FRONTEND_URL = "https://flow-finance-frontend.vercel.app";
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
